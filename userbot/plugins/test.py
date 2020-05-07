2994aa84be57aed8ab036ed6f59342d9ca2f3cb2
sethgld <64932871+sethgld@users.noreply.github.com>
2020-05-08 00:58:00 +0200

    Add files via upload

---
+++ userbot/plugins/test.py
@@ -0,0 +1,24 @@
+from telethon import events
+from datetime import datetime
+
+
+@command(pattern="^.test")
+async def _(event):
+    if event.fwd_from:
+        return
+    start = datetime.now()
+    await event.edit("**¯\_(ツ)_/¯**")
+    end = datetime.now()
+    ms = (end - start).microseconds / 1000
+    await event.edit("**¯\_(ツ)_/¯**\n{}".format(ms))   
+
+
+@command(pattern="^.ping")
+async def _(event):
+    if event.fwd_from:
+        return
+    start = datetime.now()
+    await event.edit("Pong!")
+    end = datetime.now()
+    ms = (end - start).microseconds / 1000
+    await event.edit("Pong!\n{}".format(ms))
---
+++ userbot/plugins/warnbun.py
@@ -0,0 +1,124 @@
+""".admin Plugin for @UniBorg"""
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from userbot.utils import admin_cmd
+
+
+@borg.on(admin_cmd("warn1"))
+async def _(event):
+    if event.fwd_from:
+        return
+    mentions = "`WARN  1/3 avvertimento...\nATTENTO!...\nMOTIVO: Sei ebreo"
+    chat = await event.get_input_chat()
+    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
+        mentions += f""
+    reply_message = None
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        await reply_message.reply(mentions)
+    else:
+        await event.reply(mentions)
+    await event.delete()
+
+""".admin Plugin for @UniBorg"""
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from uniborg.util import admin_cmd
+
+
+@borg.on(admin_cmd("warn2"))
+async def _(event):
+    if event.fwd_from:
+        return
+    mentions = "`WARN  2/3  avvertimento...\nATTENTO!...\nMOTIVO: rubi caramelle ai bambini`"
+    chat = await event.get_input_chat()
+    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
+        mentions += f""
+    reply_message = None
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        await reply_message.reply(mentions)
+    else:
+        await event.reply(mentions)
+    await event.delete()
+
+""".admin Plugin for @UniBorg"""
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from uniborg.util import admin_cmd
+
+
+@borg.on(admin_cmd("warn3"))
+async def _(event):
+    if event.fwd_from:
+        return
+    mentions = "`WARN  3/3...\nBAN!!!...\nMOTIVO BAN: hai rotto il cazzo`"
+    chat = await event.get_input_chat()
+    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
+        mentions += f""
+    reply_message = None
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        await reply_message.reply(mentions)
+    else:
+        await event.reply(mentions)
+    await event.delete()
+
+""".admin Plugin for @UniBorg"""
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from uniborg.util import admin_cmd
+
+
+@borg.on(admin_cmd("warn0"))
+async def _(event):
+    if event.fwd_from:
+        return
+    mentions = "`WARN RESETTATI...\nHai  0/3  warn`"
+    chat = await event.get_input_chat()
+    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
+        mentions += f""
+    reply_message = None
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        await reply_message.reply(mentions)
+    else:
+        await event.reply(mentions)
+    await event.delete()
+
+
+@borg.on(admin_cmd("ocb"))
+async def _(event):
+    if event.fwd_from:
+        return
+    mentions = "**ATTENZIONE..\n\nBatteria Sotto Il 10%, Per Favore Ricarica**"
+    chat = await event.get_input_chat()
+    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
+        mentions += f""
+    reply_message = None
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        await reply_message.reply(mentions)
+    else:
+        await event.reply(mentions)
+    await event.delete()
+
+@borg.on(admin_cmd("fw"))
+async def _(event):
+    if event.fwd_from:
+        return
+    mentions = "`FLOODWAIT ERROR:\nMotivo:telethon.errors.rpcerrorlist.FloodWaitError: Attendi 546578265716823 secondi (causato da troppi messaggi)`"
+    chat = await event.get_input_chat()
+    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
+        mentions += f""
+    reply_message = None
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        await reply_message.reply(mentions)
+    else:
+        await event.reply(mentions)
+    await event.delete()
