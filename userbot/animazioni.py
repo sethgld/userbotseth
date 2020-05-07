9b39cb8e1017489cd0d74985ce4807d1c9a8e84f
sethgld <64932871+sethgld@users.noreply.github.com>
2020-05-08 00:23:06 +0200

    Add files via upload

---
+++ userbot/animazioni.py
@@ -0,0 +1,217 @@
+﻿"""
+Available Commands:
+.bombs
+.fuck
+.kiss
+.love
+.pornhub
+.sex
+.sexy
+"""
+
+from telethon import events
+
+from userbot.utils import admin_cmd
+
+import asyncio
+
+
+
+@borg.on(admin_cmd(pattern=f"bombs", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+       
+ 
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n")
+    await asyncio.sleep(0.5)
+    await event.edit("ðﾟﾒﾣðﾟﾒﾣðﾟﾒﾣðﾟﾒﾣ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n")
+    await asyncio.sleep(0.5)
+    await event.edit("▪️▪️▪️▪️ \nðﾟﾒﾣðﾟﾒﾣðﾟﾒﾣðﾟﾒﾣ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n")
+    await asyncio.sleep(0.5)
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \nðﾟﾒﾣðﾟﾒﾣðﾟﾒﾣðﾟﾒﾣ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n")
+    await asyncio.sleep(0.5)
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \nðﾟﾒﾣðﾟﾒﾣðﾟﾒﾣðﾟﾒﾣ \n▪️▪️▪️▪️ \n")
+    await asyncio.sleep(0.5)
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \nðﾟﾒﾣðﾟﾒﾣðﾟﾒﾣðﾟﾒﾣ \n")
+    await asyncio.sleep(1)
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \nðﾟﾒﾥðﾟﾒﾥðﾟﾒﾥðﾟﾒﾥ \n")
+    await asyncio.sleep(0.5)
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \nðﾟﾒﾥðﾟﾒﾥðﾟﾒﾥðﾟﾒﾥ \nðﾟﾒﾥðﾟﾒﾥðﾟﾒﾥðﾟﾒﾥ \n")
+    await asyncio.sleep(0.5)
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \nðﾟﾘﾵðﾟﾘﾵðﾟﾘﾵðﾟﾘﾵ \n")
+    await asyncio.sleep(0.5)
+    await event.edit("`RIP ðﾟﾘﾵðﾟﾘﾵðﾟﾘﾵ...`")
+    await asyncio.sleep(2)
+    
+
+@borg.on(admin_cmd(pattern=f"fuck", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 0.2
+    animation_ttl = range(0, 101)
+    #input_str = event.pattern_match.group(1)
+    #if input_str == "fuck":
+    await event.edit("fuck")
+    animation_chars = [
+
+            "ðﾟﾑﾉ       ✊️",
+
+            "ðﾟﾑﾉ     ✊️",
+
+            "ðﾟﾑﾉ  ✊️",
+
+            "ðﾟﾑﾉ✊️ðﾟﾒﾦ"
+
+        ]
+
+    for i in animation_ttl:
+            await asyncio.sleep(animation_interval)
+            await event.edit(animation_chars[i % 4])
+
+
+@borg.on(admin_cmd(pattern="love", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 1.0
+    animation_ttl = range(0, 101)
+    #input_str = event.pattern_match.group(1)
+    #if input_str == "love":
+    await event.edit("love")
+    animation_chars = [
+
+            "L_",
+
+            "LO_",
+
+            "LOV_",
+            
+            "LOVE_",
+            
+            "LOVE❤",
+            
+
+        ]
+
+    for i in animation_ttl:
+            await asyncio.sleep(animation_interval)
+            await event.edit(animation_chars[i % 10])
+
+
+
+@borg.on(admin_cmd(pattern=f"kiss", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 0.2
+    animation_ttl = range(0, 101)
+    #input_str = event.pattern_match.group(1)
+    #if input_str == "kiss":
+    await event.edit("kiss")
+    animation_chars = [
+
+            "ðﾟﾤﾵ       ðﾟﾑﾰ",
+
+            "ðﾟﾤﾵ     ðﾟﾑﾰ",
+
+            "ðﾟﾤﾵ  ðﾟﾑﾰ",
+
+            "ðﾟﾤﾵðﾟﾒﾋðﾟﾑﾰ"
+
+        ]
+
+    for i in animation_ttl:
+            await asyncio.sleep(animation_interval)
+            await event.edit(animation_chars[i % 4])
+
+
+@borg.on(admin_cmd(pattern="pornhub", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 1.0
+    animation_ttl = range(0, 101)
+    #input_str = event.pattern_match.group(1)
+    #if input_str == "pornhub":
+    await event.edit("pornhub")
+    animation_chars = [
+
+            "P_",
+
+            "PO_",
+
+            "POR_",
+
+            "PORN_",
+            
+            "PORNH_",
+            
+            "PORNHU_",
+            
+           "PORNHUB_", 
+           
+           "PORNHUB",
+
+        ]
+
+    for i in animation_ttl:
+            await asyncio.sleep(animation_interval)
+            await event.edit(animation_chars[i % 10])
+
+
+@borg.on(admin_cmd(pattern=f"sex", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 0.2
+    animation_ttl = range(0, 101)
+    #input_str = event.pattern_match.group(1)
+    #if input_str == "sex":
+    await event.edit("sex")
+    animation_chars = [
+
+            "ðﾟﾤﾵ       ðﾟﾑﾰ",
+
+            "ðﾟﾤﾵ     ðﾟﾑﾰ",
+
+            "ðﾟﾤﾵ  ðﾟﾑﾰ",
+
+            "ðﾟﾤﾵðﾟﾑﾼðﾟﾑﾰ"
+
+        ]
+
+    for i in animation_ttl:
+            await asyncio.sleep(animation_interval)
+            await event.edit(animation_chars[i % 4])
+
+
+@borg.on(admin_cmd(pattern="sexy", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 1.0
+    animation_ttl = range(0, 101)
+    #input_str = event.pattern_match.group(1)
+    #if input_str == "sexy":
+    await event.edit("sexy")
+    animation_chars = [
+
+            "S_",
+
+            "SE_",
+
+            "SEX_",
+
+            "SEXY_",
+            
+            "SEXYðﾟﾑﾄ_",
+            
+            "SEXYðﾟﾑﾄ",
+            
+        ]
+
+    for i in animation_ttl:
+            await asyncio.sleep(animation_interval)
+            await event.edit(animation_chars[i % 10])
---
+++ userbot/stickersfoto.py
@@ -0,0 +1,74 @@
+import datetime
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
+import datetime
+import asyncio
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
+from userbot.events import register
+from userbot import bot, CMD_HELP
+
+from userbot import bot, CMD_HELP
+import io
+import math
+import urllib.request
+from os import remove
+from userbot.events import register
+import io
+import os
+import random
+import textwrap
+from PIL import Image, ImageDraw, ImageFont
+from telethon.tl.types import InputMessagesFilterDocument
+
+from PIL import Image
+import random
+from telethon.tl.types import DocumentAttributeFilename, MessageMediaPhoto
+from userbot import bot, CMD_HELP
+import io
+import os
+import random
+import textwrap
+from PIL import Image, ImageDraw, ImageFont
+from telethon.tl.types import InputMessagesFilterDocument
+from telethon.tl.functions.messages import GetStickerSetRequest
+from telethon.tl.types import InputStickerSetID
+from telethon.tl.types import DocumentAttributeSticker
+from userbot import CMD_HELP, ALIVE_NAME
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "I'M STUPID"
+
+
+@register(outgoing=True, pattern="^.ext1(?: |$)(.*)")
+async def _(event):
+    if event.fwd_from:
+        return  
+    if not event.reply_to_msg_id:
+       await event.edit(f"`{DEFAULTUSER}:`**Rispondi ad una foto**")
+       return
+    reply_message = await event.get_reply_message() 
+    if not reply_message.media:
+       await event.edit(f"`{DEFAULTUSER}:`**Rispondi ad una foto**")
+       return
+    chat = "@BuildStickerBot"
+    sender = reply_message.sender
+    if reply_message.sender.bot:
+       await event.edit(f"`{DEFAULTUSER}:`**Rispondi a un user, no al bot.**")
+       return
+    await event.edit(f"`{DEFAULTUSER}:`**Creo Stickers...**")
+    async with bot.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=164977173))
+              await bot.forward_messages(chat, reply_message)
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("```Please sblocca @BuildStickerBot ```")
+              return
+          if response.text.startswith("Forward"):
+             await event.edit(f"`{DEFAULTUSER}:`**privacy error**")
+          else:
+          	if response.text.startswith("Select"):
+          		await event.edit(f"`{DEFAULTUSER}:`Please vai su @DrWebBot e select la lingua.") 
+          	else: 
+          			await bot.send_file(event.chat_id, response.message.media)
